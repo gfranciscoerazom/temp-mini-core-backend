fc362b13a2e40f929de93e509b208d1e6edf2fc8
Gabriel Erazo <gfranciscoerazom@protonmail.com>
2024-04-18 18:22:44 -0500

    Updates

--- app.py
+++ app.py
@@ -55,7 +55,4 @@ app.register_blueprint(publicacion_blueprint)
 # app.register_blueprint(notificacion_blueprint)
 
 if __name__ == "__main__":
-    app.run(
-        port=8000,
-        host="0.0.0.0"
-    )
+    app.run(debug=True)
